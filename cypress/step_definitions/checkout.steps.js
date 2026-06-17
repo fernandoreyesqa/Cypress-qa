1480396dfbb68d26e6fdd822172f9663a09676f1
Fernando Reyes <gonza.fernando.565@gmail.com>
2026-06-16 21:28:29 -0400

    fix checkout.step

--- cypress/step_definitions/checkout.steps.js
+++ cypress/step_definitions/checkout.steps.js
@@ -1,6 +1,8 @@
 import { Given, When, Then, Before } from '@badeball/cypress-cucumber-preprocessor'
 import LoginPage from '../page/login/loginPage'
 import InventoryPage from '../page/inventory/inventoryPage'
+import CarroPage  from '../page/carro/carroPage'
+import CheckoutPage from '../page/checkout/checkoutPage'
 
 Before(() => {
   Cypress.on('uncaught:exception', () => false)
